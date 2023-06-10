1a8109decca3cc136609de3d82ba2346ffa42481
Ali Alizada <92538155+Nephorid@users.noreply.github.com>
2023-06-10 16:48:29 +0400

    Version : Alpha
    Programin ilk alpha sürümüdür.

---
+++ Oyun.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Test
+{
+    public class Oyun
+    {
+        public string Ad { get; set; }
+        public string ExecutablePath { get; set; }
+        public TimeSpan OynamaSuresi { get; set; }
+
+        public Oyun(string ad, string executablePath)
+        {
+            Ad = ad;
+            ExecutablePath = executablePath;
+        }
+
+        public override string ToString()
+        {
+            return Ad;
+        }
+    }
+
+}
---
+++ Resources/Languages/CustomDictionary.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+
+public class CustomDictionary : Dictionary<string, object>
+{
+    public string Settings { get; set; }
+}
